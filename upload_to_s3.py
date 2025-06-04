09d2d3558fe1fecedbf93c30b48b931cbb0d7493
Your Name <you@example.com>
2025-06-04 13:35:30 +0000

    Adding all the script for uploading and downloading the image from the S3

--- upload_to_s3.py
+++ upload_to_s3.py
@@ -1,13 +1,12 @@
-# upload_toS3_bucket.py
-# Server -1
+# server-2
 import boto3, os
 
 s3 = boto3.client('s3')
-bucket = "eagleeye-input"
-folder = "/home/ubuntu/Object-detection-in-kubernetes/images/"
+bucket = "eagleeye-output"
+folder = "/mnt/yolov5-data/output/results/"
 
 for filename in os.listdir(folder):
     if filename.lower().endswith(('.jpg', '.png', '.jpeg')):
         local_path = os.path.join(folder, filename)
-        s3.upload_file(local_path, bucket, f"input-images/{filename}")
+        s3.upload_file(local_path, bucket, f"output-images/{filename}")
         print(f"✅ Uploaded {filename} to S3")
